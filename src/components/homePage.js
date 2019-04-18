4ec217ad7aea6ff94dbb3d16cf0aad9b19d18c0e
MarSchlaefer <mschlaefer00@gmail.com>
2019-04-18 16:23:30 -0400

    Finish semantic drop down menu.

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,65 +1,12 @@
 import React, { Component } from 'react';
-import { Dropdown } from 'react-bootstrap';
+import NavMenu from './navMenu'
 
 class HomePage extends Component {
     render() {
       return (
-          <div>
-            <div className="nav-bar">
-                <span>
-                    <Dropdown>
-                        <Dropdown.Toggle variant="success" id="dropdown-basic">
-                            Games
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu>
-                            <Dropdown.Item href="#/action-1">New Game</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">Game History</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </span>
-                <span>
-                    <Dropdown>
-                        <Dropdown.Toggle variant="success" id="dropdown-basic">
-                            Teams
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu>
-                            <Dropdown.Item href="#/action-1">New Team</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">My Teams</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">All Teams</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </span>
-                <span>
-                    <Dropdown>
-                        <Dropdown.Toggle variant="success" id="dropdown-basic">
-                            Sports
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu>
-                            <Dropdown.Item href="#/action-1">New Sport</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">My Sports</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">All Sports</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </span>
-                <span>
-                    <Dropdown>
-                        <Dropdown.Toggle variant="success" id="dropdown-basic">
-                            Reports
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu>
-                            <Dropdown.Item href="#/action-1">My Reports</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">All Reports</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </span>
-            </div>
-            <div className="recent-reports-container">
-                <h1>Recent Reports</h1>
-            </div>
-            <div className="top-players-container">
-                <h1>Top Players</h1>
-            </div>
-          </div>
+          <React.Fragment>
+              <NavMenu />
+          </React.Fragment>
       );
     }
   }
---
+++ src/components/navMenu.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Dropdown, Menu } from 'semantic-ui-react'
+
+const NavMenu = () => (
+  <div>
+    <Menu attached='top'>
+      <Dropdown item text="Games"> 
+        <Dropdown.Menu>
+          <Dropdown.Item>New Game</Dropdown.Item>
+          <Dropdown.Item>Game History</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+
+      <Dropdown item text="Teams"> 
+        <Dropdown.Menu>
+          <Dropdown.Item>New Team</Dropdown.Item>
+          <Dropdown.Item>All Teams</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+
+      <Dropdown item text="Sports"> 
+        <Dropdown.Menu>
+          <Dropdown.Item>New Sport</Dropdown.Item>
+          <Dropdown.Item>All Sports</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+
+      <Dropdown item text="Reports"> 
+        <Dropdown.Menu>
+          <Dropdown.Item>New Report</Dropdown.Item>
+          <Dropdown.Item>All Reports</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+
+    </Menu>
+
+  </div>
+)
+
+export default NavMenu
