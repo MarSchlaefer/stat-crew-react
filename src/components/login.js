47539cee5fafd303cdec449131efcbb032b42502
MarSchlaefer <mschlaefer00@gmail.com>
2019-04-23 14:35:09 -0400

    Used semantic to handle form change.

--- src/components/login.js
+++ src/components/login.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter, Redirect } from 'react-router';
+import { loginUser } from './actions/user';
+import { Button, Form, Segment, Message } from 'semantic-ui-react';
+
 
 class Login extends Component {
+  state = { username: '', password: ''}
+
     render() {
       return (
         <div className="Login">
@@ -16,7 +23,17 @@ class Login extends Component {
         </div>
       );
     }
-  }
+
+    handleChange = (e, semanticInputData) => {
+      this.setState({ [semanticInputData.name]: [semanticInputData.value] })
+    }
+
+    handleLoginSubmit = () => { // semantic preventsDefault for you
+      this.props.loginUser(this.state.username, this.state.password)
+      this.setState({ username: '', password: ''})
+    }
+
+  } //end of class
   
   export default Login;
   
