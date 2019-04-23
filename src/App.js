279a7ece5f081f4cf7748038ac8d1f0f381f66b6
MarSchlaefer <mschlaefer00@gmail.com>
2019-04-23 14:33:04 -0400

    Added routing for login and home.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
+import { Route, Switch, Redirect, withRouter } from 'react-router-dom'
 import Login from './components/login';
 import HomePage from './components/homePage';
 import './App.css';
 
 class App extends Component {
   render() {
+    console.log('% APP PROPS: ', 'color: firebrick', this.props)
     return (
       <div className="App">
-        <Login />
-        <HomePage/>
+        <Switch>
+          <Route exact path='/' render={() => <Redirect to='/home' />} />
+          <Route exact path='/home' component={HomePage} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
+
+
