d5023adf2f8c2bee6cf027e5a9fc577b0357697a
MarSchlaefer <mschlaefer00@gmail.com>
2019-04-23 14:33:45 -0400

    Used semantic dropdown navbar.

--- src/components/navMenu.js
+++ src/components/navMenu.js
@@ -1,34 +1,34 @@
 import React from 'react'
 import { Dropdown, Menu } from 'semantic-ui-react'
 
-const NavMenu = () => (
+const NavMenu = (props) => (
   <div>
     <Menu secondary attached='top'>
       <Dropdown item text="Games"> 
         <Dropdown.Menu>
-          <Dropdown.Item>New Game</Dropdown.Item>
-          <Dropdown.Item>Game History</Dropdown.Item>
+          <Dropdown.Item onClick={props.handleNewGame}>New Game</Dropdown.Item>
+          <Dropdown.Item onClick={props.handleGameHistory}>Game History</Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
 
       <Dropdown item text="Teams"> 
         <Dropdown.Menu>
-          <Dropdown.Item>New Team</Dropdown.Item>
-          <Dropdown.Item>All Teams</Dropdown.Item>
+          <Dropdown.Item onClick={props.handleNewTeam}>New Team</Dropdown.Item>
+          <Dropdown.Item onClick={props.handleAllTeams}>All Teams</Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
 
       <Dropdown item text="Sports"> 
         <Dropdown.Menu>
-          <Dropdown.Item>New Sport</Dropdown.Item>
-          <Dropdown.Item>All Sports</Dropdown.Item>
+          <Dropdown.Item onClick={props.handleNewSport}>New Sport</Dropdown.Item>
+          <Dropdown.Item onClick={props.handleAllSports}>All Sports</Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
 
       <Dropdown item text="Reports"> 
         <Dropdown.Menu>
-          <Dropdown.Item>New Report</Dropdown.Item>
-          <Dropdown.Item>All Reports</Dropdown.Item>
+          <Dropdown.Item onClick={props.handleNewReport}>New Report</Dropdown.Item>
+          <Dropdown.Item onClick={props.handleAllReports}>All Reports</Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
 
