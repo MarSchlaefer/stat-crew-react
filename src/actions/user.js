38ae888528cf16706604d9e8119523699f31285c
MarSchlaefer <mschlaefer00@gmail.com>
2019-04-24 10:15:54 -0400

    Fixed typo and debugged login.

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,39 +1,36 @@
 export const loginUser = (username, password) => {
     return (dispatch) => { //thunk
         dispatch({ type: 'AUTHENTICATING_USER' })
-        fetch(`http://localhost3000/api/v1/login`, { //move this to adapter
+        fetch(`http://localhost:3000/api/v1/login`, { //move this to adapter
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                Accept: 'application/json'
+                'Accept': 'application/json'
             },
             body: JSON.stringify({
                 user: {
                     username: username,
-                    passowrd: password
+                    password: password
                 }
             })
-        })
-        .then(resp => {
+        }).then(resp => {
             if (resp.ok) {
                 return resp.json()
             } else {
                 throw resp
             }
-        })
-        .then(JSONResp => {
+        }).then(JSONResp => {
             console.log('%c INSIDE THEN', 'color: pink', JSONResp)
             localStorage.setItem('jwt', JSONResp.jwt)
             dispatch({ type: 'SET_CURRENT_USER', payload: JSONResp.user })
-        })
-        .catch(r => r.json()).then(e => dispatch({ type: 'FAILED_LOGIN', payload: e.message }))
+        }).catch(resp => resp.json().then(e => dispatch({ type: 'FAILED_LOGIN', payload: e.message })))
     }
 }
 
 export const fetchCurrentUser = () => {
     return (dispatch) => {
         dispatch(authenticatingUser()) //tells app we are fetching
-        fetch(`http://localhost3000/api/v1/profile`, { //move this to adapter
+        fetch(`http://localhost:3000/api/v1/profile`, { //move this to adapter
             method: 'GET',
             headers: {
                 Authorization: `Bearer ${localStorage.getItem('jwt')}`
